febaf8b06f0f33fb2e9fb3c3a0f4fe064b11d599
nickl- <github@jigsoft.co.za>
2014-04-30 14:26:05 +0200

    Create newe MIME type for application/json+schema

--- lib/schema_builder.rb
+++ lib/schema_builder.rb
@@ -3,4 +3,12 @@ require 'schema_builder/version'
 require 'schema_builder/writer'
 require 'schema_builder/railtie' if defined? ::Rails::Railtie
 
-module SchemaBuilder;end
+module SchemaBuilder
+  Mime::Type.register "application/schema+json", :schema
+
+  ActionController::Renderers.add :schema do |schema, options|
+    self.content_type ||= Mime::SCHEMA
+    require 'schema_builder'
+    SchemaBuilder::Writer.new.to_schema(schema.class)
+  end
+end
